1db7836468293ead3ca6cd6f98b362b49dec9890
AlbinaIrb <148874453+AlbinaIrb@users.noreply.github.com>
2023-11-20 15:21:56 +0300

    Update ingredients.php

--- ingredients.php
+++ ingredients.php
@@ -15,26 +15,38 @@ if (isset($_POST['add_to_fridge'])) {
     $ingredient_id = $_POST['ingredient_id'];
     $quantity = $_POST['quantity'];
 
-    // Проверяем, существует ли уже такой ингредиент в холодильнике пользователя
-    $stmt = $db->prepare("SELECT * FROM fridge_ingredients WHERE fridge_id IN (SELECT fridge_id FROM user_fridges WHERE user_id = :user_id) AND ingredient_id = :ingredient_id");
-    $stmt->bindParam(':user_id', $user_id);
-    $stmt->bindParam(':ingredient_id', $ingredient_id);
-    $stmt->execute();
+    try {
+        // Проверяем, существует ли уже такой ингредиент в холодильнике пользователя
+        $stmt = $db->prepare("SELECT * FROM fridge_ingredients WHERE fridge_id IN (SELECT fridge_id FROM user_fridges WHERE user_id = :user_id) AND ingredient_id = :ingredient_id");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':ingredient_id', $ingredient_id);
+        $stmt->execute();
 
-    if ($stmt->rowCount() > 0) {
-        // Если ингредиент уже существует, обновляем количество
-        $updateStmt = $db->prepare("UPDATE fridge_ingredients SET quantity = quantity + :quantity WHERE fridge_id IN (SELECT fridge_id FROM user_fridges WHERE user_id = :user_id) AND ingredient_id = :ingredient_id");
-        $updateStmt->bindParam(':user_id', $user_id);
-        $updateStmt->bindParam(':ingredient_id', $ingredient_id);
-        $updateStmt->bindParam(':quantity', $quantity);
-        $updateStmt->execute();
-    } else {
-        // Если ингредиент не существует, добавляем его
-        $insertStmt = $db->prepare("INSERT INTO fridge_ingredients (fridge_id, ingredient_id, quantity) VALUES ((SELECT fridge_id FROM user_fridges WHERE user_id = :user_id), :ingredient_id, :quantity)");
-        $insertStmt->bindParam(':user_id', $user_id);
-        $insertStmt->bindParam(':ingredient_id', $ingredient_id);
-        $insertStmt->bindParam(':quantity', $quantity);
-        $insertStmt->execute();
+        if ($stmt->rowCount() > 0) {
+            // Если ингредиент уже существует, обновляем количество
+            $updateStmt = $db->prepare("UPDATE fridge_ingredients SET quantity = quantity + :quantity WHERE fridge_id IN (SELECT fridge_id FROM user_fridges WHERE user_id = :user_id) AND ingredient_id = :ingredient_id");
+            $updateStmt->bindParam(':user_id', $user_id);
+            $updateStmt->bindParam(':ingredient_id', $ingredient_id);
+            $updateStmt->bindParam(':quantity', $quantity);
+            if ($updateStmt->execute()) {
+                echo "Ингредиент успешно обновлен!";
+            } else {
+                echo "Ошибка при обновлении ингредиента: " . implode(" ", $updateStmt->errorInfo());
+            }
+        } else {
+            // Если ингредиент не существует, добавляем его
+            $insertStmt = $db->prepare("INSERT INTO fridge_ingredients (fridge_id, ingredient_id, quantity) VALUES ((SELECT fridge_id FROM user_fridges WHERE user_id = :user_id), :ingredient_id, :quantity)");
+            $insertStmt->bindParam(':user_id', $user_id);
+            $insertStmt->bindParam(':ingredient_id', $ingredient_id);
+            $insertStmt->bindParam(':quantity', $quantity);
+            if ($insertStmt->execute()) {
+                echo "Ингредиент успешно добавлен!";
+            } else {
+                echo "Ошибка при добавлении ингредиента: " . implode(" ", $insertStmt->errorInfo());
+            }
+        }
+    } catch (PDOException $e) {
+        echo "Ошибка при выполнении запроса: " . $e->getMessage();
     }
 }
 
@@ -44,41 +56,40 @@ $ingredientsStmt->execute();
 $ingredients = $ingredientsStmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<!-- HTML-код для поиска ингредиентов и добавления в холодильник -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Поиск ингредиентов</title>
+    <!-- Подключение jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!-- Подключение jQuery UI -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <!-- Подключение вашего скрипта search.js -->
     <script src="search.js"></script>
 </head>
 <body>
-<h1>Поиск ингредиентов</h1>
-<!-- Форма для поиска ингредиентов -->
-<form action="search.php" method="get">
-    <label for="ingredient">Введите ингредиент:</label>
-    <input type="text" name="ingredient" id="ingredient" oninput="updateResults()">
-    <input type="submit" value="Поиск">
-</form>
-
-<!-- Контейнер для результатов поиска -->
-<div id="results"></div>
+<h1>Личный холодильник</h1>
 
 <!-- Форма для добавления ингредиентов в холодильник -->
 <form method="POST" action="ingredients.php" onsubmit="return addToFridge()">
     <p>Выберите ингредиент:
-        <select name="ingredient_id">
-            <?php foreach ($ingredients as $ingredient): ?>
-                <option value="<?= $ingredient['ingredient_id'] ?>"><?= $ingredient['name'] ?></option>
-            <?php endforeach; ?>
-        </select>
+        <input type="text" name="ingredient" id="addIngredient" class="autocomplete">
+        <input type="hidden" name="ingredient_id" id="selected_ingredient_id">
     </p>
     <p>Укажите количество: <input type="number" name="quantity" min="1" value="1"></p>
     <p><button type="submit" name="add_to_fridge">Добавить в холодильник</button></p>
 </form>
 
-<!-- Ваш HTML-код, например, заголовок страницы -->
+<!-- Включаем jQuery и jQuery UI -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+<!-- Подключаем ваш скрипт search.js -->
+<script src="search.js"></script>
 
 </body>
 </html>
+
