04a43d0d3bd3c70955ef6578d52e0a3552eae5ee
AlbinaIrb <148874453+AlbinaIrb@users.noreply.github.com>
2023-12-03 15:01:12 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,57 +1,20 @@
 <?php
-require "db.php";
+// Подключение к базе данных или другие необходимые настройки
 
-$data = $_POST;
+// Проверка, была ли отправлена форма
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Получение введенных данных из формы
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-if (isset($data['do_login'])) {
-    $errors = [];
-
-    $host = "localhost"; // Хост PostgreSQL сервера
-    $port = 5432; // Порт PostgreSQL сервера
-    $dbname = "users"; // Имя вашей базы данных
-    $user = "postgres"; // Имя пользователя PostgreSQL
-    $password = "080120"; // Пароль пользователя PostgreSQL
-
-    error_log("Точка 1"); // Первая точка для отладки
-
-    try {
-        $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
-        error_log("Соединение с базой данных успешно установлено"); // Точка для отладки
-    } catch (PDOException $e) {
-        echo "Ошибка подключения к базе данных: " . $e->getMessage();
-        die();
-    }
-
-    $stmt = $db->prepare("SELECT * FROM users WHERE email = :email");
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $user = $stmt->fetch();
-
-    error_log("Точка 2"); // Вторая точка для отладки
-
-    if ($user) {
-        if (password_verify($password, $user['password'])) {
-            // Пароль верный
-
-            // Начало сессии с использованием безопасной случайной строки
-            session_start([
-                'cookie_lifetime' => 86400,
-                'read_and_close' => true,
-            ]);
-            $_SESSION['user_id'] = $user['user_id'];
-
-            // Перенаправление на страницу поиска ингредиентов
-            header('Location: ingredients.php');
-            exit(); // Важно прервать выполнение кода после перенаправления
-        } else {
-            $errors[] = 'Данные введены неверно';
-        }
+    // Проверка логина и пароля (пример, требуется адаптация)
+    if ($email == "ваш_желаемый_логин" && $password == "ваш_желаемый_пароль") {
+        // Вход успешен
+        echo "Вход выполнен успешно!";
+        // Здесь вы можете выполнить необходимые действия после успешного входа
     } else {
-        $errors[] = 'Пользователь с таким email не зарегистрирован';
-    }
-
-    if (!empty($errors)) {
-        echo '<div style="color: red;">' . implode('<br>', $errors) . '</div><hr>';
+        // Неверные логин или пароль
+        echo "Неверный логин или пароль.";
     }
 }
 ?>
