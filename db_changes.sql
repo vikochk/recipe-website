7a3204f0c23630e2267a8fc26adcbf42786a4e3e
vikochk <106889226+vikochk@users.noreply.github.com>
2023-12-13 21:20:22 +0300

    Тут картинки к рецептам

---
+++ db_changes.sql
@@ -0,0 +1,199 @@
+UPDATE public.ingredients SET
+measure = 'мл.'::character varying WHERE
+ingredient_id = 48;
+
+UPDATE public.ingredients SET
+measure = 'гр.'::character varying WHERE
+ingredient_id = 72;
+
+UPDATE public.ingredients SET
+measure = 'гр.'::character varying WHERE
+ingredient_id = 111;
+
+UPDATE public.recipies SET
+photo_link = 'https://aroma-avenue.ru/wp-content/uploads/0/5/6/056558cee999731d3286ef68cae9191b.jpeg'::text WHERE
+recipe_id = 1;
+
+UPDATE public.recipies SET
+photo_link = 'https://mykaleidoscope.ru/uploads/posts/2021-09/1632333154_55-mykaleidoscope-ru-p-shokoladnii-tort-v-mikrovolnovke-krasivo-f-64.jpg'::text WHERE
+recipe_id = 2;
+
+UPDATE public.recipies SET
+photo_link = 'https://marybakery.ru/wp-content/uploads/2019/11/3B2AC7B8-B1C2-4012-8643-DDE3084723BF.jpeg'::text WHERE
+recipe_id = 3;
+
+UPDATE public.recipies SET
+photo_link = 'https://mykaleidoscope.ru/uploads/posts/2021-09/1632761658_54-mykaleidoscope-ru-p-tort-bananovii-ostrov-krasivo-foto-65.jpg'::text WHERE
+recipe_id = 4;
+
+UPDATE public.recipies SET
+photo_link = 'https://candiland.ru/wp-content/uploads/2018/08/Tort-Zebra-kofejnyj-na-manke.jpg'::text WHERE
+recipe_id = 5;
+
+UPDATE public.recipies SET
+photo_link = 'http://klublady.ru/uploads/posts/2022-02/1644683131_18-klublady-ru-p-tort-napoleon-klassicheskii-foto-19.jpg'::text WHERE
+recipe_id = 6;
+
+UPDATE public.recipies SET
+photo_link = 'https://lobsterhouse.ru/wp-content/uploads/1/7/8/178024bb508fe76faba7301d4d1d103e.jpeg'::text WHERE
+recipe_id = 7;
+
+UPDATE public.recipies SET
+photo_link = 'https://ramsaygordon.ru/wp-content/uploads/2022/02/2991233310-7780674.jpg'::text WHERE
+recipe_id = 8;
+
+UPDATE public.recipies SET
+photo_link = 'http://ladyinlife.ru/wp-content/uploads/2015/12/smetannik-retsept06.jpg'::text WHERE
+recipe_id = 9;
+
+UPDATE public.recipies SET
+photo_link = 'https://maroseyka4.ru/wp-content/uploads/2021/11/5ceff785bee61.jpg'::text WHERE
+recipe_id = 10;
+
+UPDATE public.recipies SET
+photo_link = 'https://img.delo-vcusa.ru/2020/02/lenivye-hachapuri-1200x800.jpg'::text WHERE
+recipe_id = 11;
+
+UPDATE public.recipies SET
+photo_link = 'https://mykaleidoscope.ru/uploads/posts/2020-01/1579919084_13-p-torti-napoleon-18.jpg'::text WHERE
+recipe_id = 12;
+
+UPDATE public.recipies SET
+photo_link = 'https://avatars.dzeninfra.ru/get-zen_doc/2815454/pub_5ed542388b02c372bc45eb82_5ed5425908aab5057a13215e/scale_1200'::text WHERE
+recipe_id = 13;
+
+UPDATE public.recipies SET
+photo_link = 'https://avatars.dzeninfra.ru/get-zen-vh/9875806/2a00000189c1b95634a2ca0c1cff8ddfc627/orig'::text WHERE
+recipe_id = 14;
+
+UPDATE public.recipies SET
+photo_link = 'https://cafesahara.ru/upload/resize_cache/iblock/a77/1200_1200_1/a770a92f1ff3b80cdfcc2da0cb61e1bf.jpg'::text WHERE
+recipe_id = 15;
+
+UPDATE public.recipies SET
+photo_link = 'https://zen-mn.ru/wp-content/uploads/2021/11/1629586805_52-mykaleidoscope-ru-p-blini-na-moloke-i-vode-vkusnyashki-krasivo-61.jpg'::text WHERE
+recipe_id = 16;
+
+UPDATE public.recipies SET
+photo_link = 'https://na-bludce.ru/wp-content/uploads/2022/08/oladi-na-kefire.jpg'::text WHERE
+recipe_id = 17;
+
+UPDATE public.recipies SET
+photo_link = 'https://www.photorecept.ru/wp-content/uploads/2019/11/8-1-1300x827.jpg'::text WHERE
+recipe_id = 18;
+
+UPDATE public.recipies SET
+photo_link = 'https://smak.ua/i/72/08/5/72085/image_main/8f8cc380bdad7490cfab9ec16bda4fec-resize_crop_1Xquality_100Xallow_enlarge_0Xw_1200Xh_630.jpg'::text WHERE
+recipe_id = 19;
+
+UPDATE public.recipies SET
+photo_link = 'https://domikexpert.ru/wp-content/uploads/3/c/c/3ccca243406beb6d641fdb3c7f5fd673.jpeg'::text WHERE
+recipe_id = 20;
+
+UPDATE public.recipies SET
+photo_link = 'https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663684878_54-mykaleidoscope-ru-p-draniki-so-smetanoi-yeda-krasivo-60.jpg'::text WHERE
+recipe_id = 21;
+
+UPDATE public.recipies SET
+photo_link = 'https://attuale.ru/wp-content/uploads/2017/09/chto-mozhno-bystro-ispech-k-chaju-3.jpg'::text WHERE
+recipe_id = 22;
+
+UPDATE public.recipies SET
+photo_link = 'https://receptisalatov.com/content/recipes/1002/main_b_pirojki-s-kapustoy-v-duhovke.jpg'::text WHERE
+recipe_id = 23;
+
+UPDATE public.recipies SET
+photo_link = 'https://sun9-57.userapi.com/c857620/v857620141/107906/Xk7dolE_g4I.jpg'::text WHERE
+recipe_id = 24;
+
+UPDATE public.recipies SET
+photo_link = 'https://i.pinimg.com/originals/f5/9b/7a/f59b7a3f1735638b6fb8d3b4e02197fd.jpg'::text WHERE
+recipe_id = 25;
+
+UPDATE public.recipies SET
+photo_link = 'https://kartinki.pics/uploads/posts/2022-12/1671724772_kartinkin-net-p-kartinki-pitstsi-pepperoni-vkontakte-1.jpg'::text WHERE
+recipe_id = 26;
+
+UPDATE public.recipies SET
+photo_link = 'https://bye-bye-calories.ru/wp-content/uploads/1/8/3/183cf2ed6df6575e45071d14f0f9a115.jpeg'::text WHERE
+recipe_id = 27;
+
+UPDATE public.recipies SET
+photo_link = 'http://burbonpub.ru/wp-content/uploads/2020/11/eb4brvqgteq.jpg'::text WHERE
+recipe_id = 28;
+
+UPDATE public.recipies SET
+photo_link = 'https://gotovka24.ru/wp-content/uploads/2022/08/7c4a1dc2.jpg'::text WHERE
+recipe_id = 29;
+
+UPDATE public.recipies SET
+photo_link = 'https://4deneg.ru/wp-content/uploads/0/f/0/0f0470ae04770fd30231d1d109b2b4cd.jpeg'::text WHERE
+recipe_id = 30;
+
+UPDATE public.recipies SET
+photo_link = 'https://malinika.ru/wp-content/uploads/4/3/1/431d5ccb887576092b9f64116bd5d68c.jpeg'::text WHERE
+recipe_id = 31;
+
+UPDATE public.recipies SET
+photo_link = 'https://klassicheskij-recept.ru/wp-content/uploads/2023/08/8612.jpeg'::text WHERE
+recipe_id = 32;
+
+UPDATE public.recipies SET
+photo_link = 'https://i.ytimg.com/vi/Z3I7lzXOZAc/maxresdefault.jpg'::text WHERE
+recipe_id = 33;
+
+UPDATE public.recipies SET
+photo_link = 'https://nobelvape.ru/upload/iblock/7b8/7b852fc59fcc966b9df8267c6b9bf0c8.jpg'::text WHERE
+recipe_id = 34;
+
+UPDATE public.recipies SET
+photo_link = 'https://mykaleidoscope.ru/uploads/posts/2021-10/1634757191_4-mykaleidoscope-ru-p-shokoladnaya-kolbasa-4.jpg'::text WHERE
+recipe_id = 35;
+
+UPDATE public.recipies SET
+photo_link = 'http://n1s1.hsmedia.ru/bf/67/a9/bf67a9f35762e1374f3bdd89f0adf6fe/1718x1280_0xac120003_15105397531610615429.jpg'::text WHERE
+recipe_id = 36;
+
+UPDATE public.recipies SET
+photo_link = 'https://static22.tgcnt.ru/posts/_0/ac/ac0d083429ade6d5bfecaaa0dfe71bdd.jpg'::text WHERE
+recipe_id = 37;
+
+UPDATE public.recipies SET
+photo_link = 'https://i.pinimg.com/originals/15/98/4b/15984b530ef2afac5bffe864dd1f924c.jpg'::text WHERE
+recipe_id = 38;
+
+UPDATE public.recipies SET
+photo_link = 'https://food.pibig.info/uploads/posts/2023-07/1689619968_food-pibig-info-p-makaroni-s-shampinonami-v-smetane-pinteres-48.jpg'::text WHERE
+recipe_id = 39;
+
+UPDATE public.recipies SET
+photo_link = 'https://pigmine.ru/wp-content/uploads/a/7/8/a78664152560c86780dc0e2dc55bd4bd.jpeg'::text WHERE
+recipe_id = 40;
+
+UPDATE public.recipies SET
+photo_link = 'https://simpleslim.ru/wp-content/uploads/2020/09/juicy-delicious-meat-cutlets-on-a-wooden-table-in-pbx4pc6.jpg'::text WHERE
+recipe_id = 41;
+
+UPDATE public.recipies SET
+photo_link = 'https://bye-bye-calories.ru/wp-content/uploads/a/0/7/a071cfe3711553e75a671108ccf41efc.jpeg'::text WHERE
+recipe_id = 42;
+
+UPDATE public.recipies SET
+photo_link = 'https://i.mycdn.me/i?r=BDGvRlXWcXJosdiHOqVV3e-HrEA-AhqaURwd9diMEnOTiHYlJwTiiFEnLluUl0nC9ZA'::text WHERE
+recipe_id = 43;
+
+UPDATE public.recipies SET
+photo_link = 'http://puzzlefoods.ru/wa-data/public/shop/products/61/19/1961/images/2902/2902.750x0.jpg'::text WHERE
+recipe_id = 44;
+
+UPDATE public.recipies SET
+photo_link = 'https://eda-land.ru/images/article/orig/2018/12/kak-prigotovit-kurinyj-shnicel-v-panirovke-4.jpg'::text WHERE
+recipe_id = 45;
+
+UPDATE public.recipies SET
+photo_link = 'https://static.mk.ru/upload/entities/2022/12/16/21/articles/facebookPicture/40/7e/aa/9d/259672581b178837addfa0d86bf35917.jpg'::text WHERE
+recipe_id = 46;
+
+UPDATE public.recipies SET
+photo_link = 'https://pigmine.ru/wp-content/uploads/c/7/f/c7f17343702b6ee0b1643a4151383733.jpeg'::text WHERE
+recipe_id = 47;
